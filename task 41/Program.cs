d43736405cf10cc3cb2a16cfa4148a2e41a9670d
yakov kuzin <kuzin_js@mail.ru>
2022-08-27 17:18:46 +0300

    Task41 added

--- task 41/Program.cs
+++ task 41/Program.cs
@@ -1,5 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-sdfsdfsdfsdfsdfsd
-sdfsdfsdfsdfsdfsdsdfsdfsdf
-dsfsdf
-sdfsdfsfsd
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+using System;
+
+class Programm
+{
+    static int[] CreatArray(string Array)
+    {
+        string[] nums = Array.Split(",", StringSplitOptions.RemoveEmptyEntries);
+        int[] numb = new int[nums.Length];
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            numb[i] = int.Parse(nums[i]);
+        }
+        return numb;
+    }
+
+    static int PositiveNumb(int[] array)
+    {
+        int count = 0;
+        foreach (var item in array)
+        {
+            if (item > 0) count++;
+        }
+        return count;
+    }
+
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите элементы массива через запятую. По окончании ввода нажмите Enter");
+        string elements = Console.ReadLine();
+        Console.WriteLine("Количество чисел больше нуля = " + PositiveNumb(CreatArray(elements)));
+    }
+}
